e23478ff0e1f2ecde11821c2fe7842f681c9a512
mutouzha22-spec <mutouzha22@gmail.com>
2026-07-12 16:23:51 +0800

    v1.9.1 优化十字星拖尾与粒子

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4212033914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3015594992" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
