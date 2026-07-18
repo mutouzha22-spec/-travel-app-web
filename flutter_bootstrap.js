396b4a91f126764a02c34fe956046d47f5630f14
mutouzha22-spec <mutouzha22@gmail.com>
2026-07-18 17:46:06 +0800

    v1.10.0 小猴正式台词与流星解锁

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "427288936" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3122970600" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
