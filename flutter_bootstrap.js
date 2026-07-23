0a99a4cf5044ba2f23c7a80556dced44baa4adac
mutouzha22-spec <mutouzha22@gmail.com>
2026-07-23 13:43:41 +0800

    v1.18.0 接香蕉小游戏与互动布局优化

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "73894880" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2844856360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
