43ecb238e8e63dbdb051bcc54825f83117151a1c
mutouzha22-spec <mutouzha22@gmail.com>
2026-08-08 11:29:40 +0800

    v1.24.0 修复调色按钮点击无反应

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1173226550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4025632348" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
